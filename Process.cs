e4c9636111d32767677775a3c9ccc3a5c75fd44c
NguyenAn666 <97713637+NguyenAn666@users.noreply.github.com>
2022-08-31 10:35:35 +0700

    Add files via upload

---
+++ Process.cs
@@ -0,0 +1,44 @@
+﻿namespace GeometryExample
+
+{
+    internal class Process
+    {
+        
+
+        static void Main(string[] args)
+        {
+            float Radius;
+            float PI = 3.1415926535f;
+            double BaseArea;
+            double LateralArea;
+            double TotalArea;
+            double Volume;
+            int Height;
+
+
+            Console.WriteLine("Enter the dimenstion of the cylinder");
+
+            Console.Write("Radius");
+            Radius = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Height");
+            Height = (int)Convert.ToSingle(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("Cylinder Characteristics");
+            
+
+            Console.Write("Base");
+            Console.WriteLine( Radius * Radius * Math.PI );
+
+            Console.Write("Lateral");
+            Console.WriteLine( 2 * Math.PI * Radius * Height );
+
+            Console.Write("Total");
+            Console.WriteLine(2 * Math.PI * Radius * (Height + Radius));
+
+            Console.Write("Volume");
+            Console.WriteLine(Math.PI * Radius * Radius * Height);
+        }
+    }
+}
